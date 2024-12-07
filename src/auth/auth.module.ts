def322eb4158bd67b3481ec8fd65403c75723b5a
onursahal <sahalonur@gmail.com>
2024-12-07 19:27:43 +0300

    feat(auth): jwt config added to auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+    }),
+  ],
   providers: [AuthService, AuthResolver],
 })
 export class AuthModule {}
